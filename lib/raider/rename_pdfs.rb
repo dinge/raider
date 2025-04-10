0539458a62c90b7ede5d20b5011f369359a7fa42
Lars Gollnow <lg@megorei.com>
2025-04-11 01:20:10 +0200

    refactor

--- lib/raider/rename_pdfs.rb
+++ lib/raider/rename_pdfs.rb
@@ -14,7 +14,8 @@ module Raider
 
     def process_files
       # pdf_files.shuffle[0, 3].each { |pdf| process_file(pdf) }
-      pdf_files[0, 3].each { |pdf| process_file(pdf) }
+      # pdf_files[0, 3].each { |pdf| process_file(pdf) }
+      pdf_files.each { |pdf| process_file(pdf) }
     end
 
     def process_file(pdf)
@@ -23,6 +24,7 @@ module Raider
       return unless img && File.exist?(img)
 
       analysis = @llm_handler.analyze_document(img)
+      log_response(pdf, analysis)
       output_debug(analysis) if @config.debug
 
       new_name = @pdf_processor.generate_filename(analysis)
@@ -42,6 +44,22 @@ module Raider
     def output_debug(analysis)
       puts "\nDocument Analysis:"
       puts JSON.pretty_generate(analysis)
+    end
+
+    def log_response(file_path, response)
+      FileUtils.mkdir_p('logs')
+      handler_name = @config.provider.to_s
+      log_file = "logs/analyze_document-#{handler_name}.log"
+
+      entry = {
+        timestamp: Time.now.iso8601,
+        file_path:,
+        response: response
+      }
+
+      File.open(log_file, 'a') do |f|
+        f.puts [entry].to_yaml
+      end
     end
   end
 end
