9cb7ecd2655f222be5f2d86472c8398bc1c0656e
Lars Gollnow <lg@megorei.com>
2025-07-25 19:06:21 +0200

    add vcr

--- lib/raider.rb
+++ lib/raider.rb
@@ -13,7 +13,7 @@ require 'debug'
 
 
 module Raider
-  VERSION = "0.1.6"
+  VERSION = "0.1.7"
 
   class << self
     attr_accessor :logger
--- lib/raider/agents/base.rb
+++ lib/raider/agents/base.rb
@@ -14,7 +14,11 @@ module Raider
         @provider = provider
         @agent_runner = agent_runner
         @input = input
-        @agent_context = Raider::Utils::AgentContext.new(provider: @provider.provider_ident, llm: @llm.llm_ident, input:,  tasks: [], data: {})
+        @agent_context = Raider::Utils::AgentContext.new(provider: @provider.provider_ident,
+                                                         llm: @llm.llm_ident,
+                                                         input:,
+                                                         tasks: [],
+                                                         data: {})
       end
 
       def process(&proc)
@@ -24,9 +28,24 @@ module Raider
 
       def run_task(task_ident, **args)
         task = app.create_task(task_ident, llm: nil, provider: nil, agent: self)
-        task.process(**args)
+
+        WebMock.enable!
+        VCR.use_cassette(build_vcr_key(task_ident), record: :new_episodes) do
+          task.process(**args)
+        end
+        WebMock.disable!
+
         agent_context.tasks << { task_ident.to_sym => task.context.to_hash }
         task.task_runner
+      end
+
+      def build_vcr_key(task_ident)
+        [
+          app.app_ident,
+          app.context.vcr_key.version_key,
+          app.context.vcr_key.source_ident,
+          [task_ident, @llm.llm_ident, app.context.vcr_key.reprocess_ident].join('--')
+         ].join('/')
       end
     end
   end
